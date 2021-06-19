6e8fb92ff1e8ca7ec251da7d1d4c52d2ae9d8a4a
chenjc0317 <chenjc0317@163.com>
2021-06-19 09:16:49 +0800

    1.0.5

--- Example/Pods/Target Support Files/ACSwiftTool/ACSwiftTool-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ACSwiftTool : NSObject
-@end
-@implementation PodsDummy_ACSwiftTool
-@end
--- Example/Pods/Target Support Files/Pods-ACSwiftTool_Example/Pods-ACSwiftTool_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ACSwiftTool_Example : NSObject
-@end
-@implementation PodsDummy_Pods_ACSwiftTool_Example
-@end
