b7f6e514bd489260f9bc97a441f581307832524b
chenjc0317 <chenjc0317@163.com>
2021-06-22 14:13:12 +0800

    1.2.0版本更新：UIview+Extension中增加可在xib设置的属性

--- ACSwiftTool/Classes/Core/UIKit/UIView+SwiftTool.swift
+++ ACSwiftTool/Classes/Core/UIKit/UIView+SwiftTool.swift
@@ -170,6 +170,7 @@ extension UIView {
 
 extension UIView {
     /// 设置圆角
+    @IBInspectable
     public var cornerRadius: CGFloat {
         set {
             self.layer.masksToBounds = true
@@ -177,6 +178,80 @@ extension UIView {
         }
         get {
             return self.layer.cornerRadius
+        }
+    }
+    /// 边框宽度
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    /// 边框颜色
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+    // 设置阴影
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
         }
     }
     /// 指定位置圆角
